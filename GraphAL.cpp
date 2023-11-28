accc89f8bed67097cbd0a75d833a94212a003e65
Emanuelle <emanuellemarreira@gmail.com>
2023-11-28 11:05:25 -0400

    identacao

--- GraphAL.cpp
+++ GraphAL.cpp
@@ -4,108 +4,130 @@
 using namespace std;
 
 typedef unsigned int Vertex;
-class ItemVertex{
-public: 
+class ItemVertex
+{
+public:
     unsigned int value;
-    ItemVertex(){}
-    ItemVertex(unsigned int value): value(value){};
+    ItemVertex() {}
+    ItemVertex(unsigned int value) : value(value){};
 };
 
-template<typename T>
-class Graph{
+template <typename T>
+class GraphAL
+{
 private:
     unsigned int num_vertices;
     unsigned int num_edges;
-    list<T> *adj; //pra alocar dinamicamente
+    list<T> *adj;
+
 public:
-    Graph(unsigned int);
-    ~Graph();
+    GraphAL(unsigned int);
+    ~GraphAL();
     void add_edge(Vertex, Vertex);
     void remove_edge(Vertex, Vertex);
-    list<T> get_adj(Vertex v){ return adj[v]; }
-    unsigned int get_num_vertices(){return num_vertices;}
-    unsigned int get_num_edges(){return num_edges;}
+    list<T> get_adj(Vertex v) { return adj[v]; }
+    unsigned int get_num_vertices() { return num_vertices; }
+    unsigned int get_num_edges() { return num_edges; }
 };
 
-template<typename T>
-Graph<T>::Graph(unsigned int num_vertices): num_vertices(num_vertices){
+template <typename T>
+GraphAL<T>::GraphAL(unsigned int num_vertices) : num_vertices(num_vertices)
+{
     adj = new list<ItemVertex>[num_vertices];
 }
-template<typename T>
-Graph<T>::~Graph(){
-    for (unsigned int u = 0; u < num_vertices; ++u){
-        adj[u].clear(); //pega uma lista que ta na posicao i e apaga usando clear pq adj é uma list
+
+template <typename T>
+GraphAL<T>::~GraphAL()
+{
+    for (unsigned int u = 0; u < num_vertices; ++u)
+    {
+        adj[u].clear();
     }
-    delete[] adj; //como adj é uma list
+    delete[] adj;
     adj = nullptr;
-    num_edges =0;
+    num_edges = 0;
     num_vertices = 0;
-    //cout << "\nbye graph\n";
 }
 
-template<typename T>
-void Graph<T>::add_edge(Vertex u, Vertex v){
+template <typename T>
+void GraphAL<T>::add_edge(Vertex u, Vertex v)
+{
     ItemVertex item_Vertex_v{v};
     adj[u].push_back(item_Vertex_v);
     ItemVertex item_Vertex_u{u};
-    adj[v].push_back(item_Vertex_u);//como nao é direcionado tem que fazer esse vice-versa
+    adj[v].push_back(item_Vertex_u);
     num_edges++;
 }
 
-template<typename T>
-void Graph<T>::remove_edge(Vertex u, Vertex v){
+template <typename T>
+void GraphAL<T>::remove_edge(Vertex u, Vertex v)
+{
     ItemVertex item_Vertex_v{v};
     ItemVertex item_Vertex_u{u};
     list<ItemVertex> &lista_u = adj[u];
-    for( auto itr = lista_u.begin(); itr!= lista_u.end(); ++itr){
-        if(itr->value == item_Vertex_v.value){
-            itr = lista_u.erase( itr );
+    for (auto itr = lista_u.begin(); itr != lista_u.end(); ++itr)
+    {
+        if (itr->value == item_Vertex_v.value)
+        {
+            itr = lista_u.erase(itr);
         }
     }
     list<ItemVertex> &lista_v = adj[v];
-    for( auto itr2 = lista_v.begin(); itr2!= lista_v.end(); ++itr2){
-        if(itr2->value == item_Vertex_u.value){
-            itr2 = lista_v.erase( itr2 );
+    for (auto itr2 = lista_v.begin(); itr2 != lista_v.end(); ++itr2)
+    {
+        if (itr2->value == item_Vertex_u.value)
+        {
+            itr2 = lista_v.erase(itr2);
         }
     }
     num_edges--;
 }
 
 template <typename T>
-void input_graph(Graph<T> &g, unsigned int num_edges){
+void input_GraphAL(GraphAL<T> &g, unsigned int num_edges)
+{
     Vertex u = 0;
     Vertex v = 0;
-    for(unsigned int i = 0; i<num_edges; ++i){
+    for (unsigned int i = 0; i < num_edges; ++i)
+    {
         cin >> u >> v;
         g.add_edge(u, v);
     }
 }
+
 template <typename T>
-void display_list(list<T> &lst) {
-	for (auto item_Vertex : lst) {
-		cout << item_Vertex.value << ", ";
-	}
-	cout << endl;
+void display_list(list<T> &lst)
+{
+    for (auto item_Vertex : lst)
+    {
+        cout << item_Vertex.value << ", ";
+    }
+    cout << endl;
 }
+
 template <typename T>
-void display_graph(Graph<T> &g){
-    for(unsigned int v = 0; v < g.get_num_vertices(); v++){
+void display_GraphAL(GraphAL<T> &g)
+{
+    for (unsigned int v = 0; v < g.get_num_vertices(); v++)
+    {
         cout << "v[" << v << "]: ";
-		list<T> lst = g.get_adj(v);
-		display_list( lst );
+        list<T> lst = g.get_adj(v);
+        display_list(lst);
     }
 }
-int main(){
+
+int main()
+{
     unsigned int num_vertices = 0;
     unsigned int num_edges = 0;
-    cin>> num_vertices >> num_edges;
-    cout << "num_vertices: "<< num_vertices <<endl;
-    cout << "num_edges: "<< num_edges <<endl;
-    Graph<ItemVertex> g{num_vertices};
-    input_graph(g, num_edges);
-    display_graph(g);
-    //g.remove_edge(2,3);
-    //cout<<"removendo..."<<endl;
-    //display_graph(g);
+    cin >> num_vertices >> num_edges;
+    cout << "num_vertices: " << num_vertices << endl;
+    cout << "num_edges: " << num_edges << endl;
+    GraphAL<ItemVertex> g{num_vertices};
+    input_GraphAL(g, num_edges);
+    display_GraphAL(g);
+    // g.remove_edge(2,3);
+    // cout<<"removendo..."<<endl;
+    // display_GraphAL(g);
     return 0;
 }
